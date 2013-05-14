ca7a8800565a96c0b60f08daee3f72dcf23f38c2
Kevin Manalo <kmanalo3@ubuntu-latitude-E6430.(none)>
2013-05-14 17:37:33 -0400

    Many changes, but mostly in providing the computation of fill-in

--- main.F90
+++ main.F90
@@ -4,11 +4,140 @@ program sparse
 use sparse_header_debug
 use depletion_header
 use depletion
-type(SparseCsrComplexDbg) :: A
-type(SparseCsrRealDbg) :: B
+!type(SparseCsrComplexDbg) :: ! A
+type(SparseCsrRealDbg) :: B, C, A, R
+real(8), allocatable :: rvalues(:), cvalues(:)
+integer, allocatable :: nzrows(:), nzcols(:)
+integer :: nnzr, nnzc
 !type(SparseCsrComplex) :: fill  ! fill-in matrix
 
 print *, 'sparse matrix tester'
+
+!===============================================================================
+! R Matrix Test
+!===============================================================================
+
+  call R % init (  6, 6,  15 )
+
+  R % row_ptr(1) = 1;
+  R % columns(1) = 1; R %  values(1) = 5;
+
+  R % row_ptr(2) = 2;
+  R % columns(2) = 2; R %  values(2) = 2;
+  R % columns(3) = 3; R %  values(3) = -1;
+  R % columns(4) = 4; R %  values(4) = 2;
+
+  R % row_ptr(3) = 5;
+  R % columns(5) = 3; R %  values(5) = 3;
+
+  R % row_ptr(4) = 6;
+  R % columns(6) = 1; R %  values(6) = -2;
+  R % columns(7) = 4; R %  values(7) = 1;
+  R % columns(8) = 5; R %  values(8) = 1;
+
+  R % row_ptr(5) = 9;
+  R % columns(9)  = 1; R %  values(9)  = -1;
+  R % columns(10) = 4; R %  values(10) = -1;
+  R % columns(11) = 5; R %  values(11) = 2;
+  R % columns(12) = 6; R %  values(12) = -3;
+
+  R % row_ptr(6) = 13;
+  R % columns(13) = 1; R %  values(13) = -1;
+  R % columns(14) = 2; R %  values(14) = -1;
+  R % columns(15) = 6; R %  values(15) = 6;
+
+  R % row_ptr(7) = 16;
+
+  call R % fill_in
+  allocate(rvalues(R % n), nzrows(R % n))
+  allocate(cvalues(R % n), nzcols(R % n))
+  call R % col_below_diag(1, rvalues, nzrows, nnzr) 
+  call R % row_rgtof_diag(1, cvalues, nzcols, nnzc) 
+  print*, '--- 1 ---'          
+  print*, (rvalues(i),i=1,nnzr)
+  print*, (nzrows(i),i=1,nnzr)
+  print*, (cvalues(i),i=1,nnzc)
+  print*, (nzcols(i),i=1,nnzc)
+  call R % col_below_diag(2, rvalues, nzrows, nnzr) 
+  call R % row_rgtof_diag(2, cvalues, nzcols, nnzc) 
+  print*, '--- 2 ---'          
+  print*, (rvalues(i),i=1,nnzr)
+  print*, (nzrows(i),i=1,nnzr)
+  print*, (cvalues(i),i=1,nnzc)
+  print*, (nzcols(i),i=1,nnzc)
+  call R % col_below_diag(3, rvalues, nzrows, nnzr) 
+  call R % row_rgtof_diag(3, cvalues, nzcols, nnzc) 
+  print*, '--- 3 ---'          
+  print*, (rvalues(i),i=1,nnzr)
+  print*, (nzrows(i),i=1,nnzr)
+  print*, (cvalues(i),i=1,nnzc)
+  print*, (nzcols(i),i=1,nnzc)
+  call R % col_below_diag(4, rvalues, nzrows, nnzr) 
+  call R % row_rgtof_diag(4, cvalues, nzcols, nnzc) 
+  print*, '--- 4 ---'          
+  print*, (rvalues(i),i=1,nnzr)
+  print*, (nzrows(i),i=1,nnzr)
+  print*, (cvalues(i),i=1,nnzc)
+  print*, (nzcols(i),i=1,nnzc)
+  call R % col_below_diag(5, rvalues, nzrows, nnzr) 
+  call R % row_rgtof_diag(5, cvalues, nzcols, nnzc) 
+  print*, '--- 5 ---'          
+  print*, (rvalues(i),i=1,nnzr)
+  print*, (nzrows(i),i=1,nnzr)
+  print*, (cvalues(i),i=1,nnzc)
+  print*, (nzcols(i),i=1,nnzc)
+  call R % col_below_diag(6, rvalues, nzrows, nnzr) 
+  call R % row_rgtof_diag(6, cvalues, nzcols, nnzc) 
+  print*, '--- 6 ---'          
+  print*, (rvalues(i),i=1,nnzr)
+  print*, (nzrows(i),i=1,nnzr)
+  print*, (cvalues(i),i=1,nnzc)
+  print*, (nzcols(i),i=1,nnzc)
+  stop
+
+  call C % init ( 10, 10, 36 )
+
+  C % row_ptr(1) = 1;
+  C % columns(1) = 1; C % columns(2) = 3; C % columns(3) = 8
+
+  C % row_ptr(2) = 4;
+  C % columns(4) = 2; C % columns(5) = 5; C % columns(6) = 10
+
+  C % row_ptr(3) = 7;
+  C % columns(7) = 1; C % columns(8) = 3; C % columns(9) = 7
+
+  C % row_ptr(4) = 10;
+  C % columns(10) = 4; C % columns(11) = 9; C % columns(12) = 10
+
+  C % row_ptr(5) = 13;
+  C % columns(13) = 2; C % columns(14) = 5; 
+  C % columns(15) = 9; C % columns(16) = 10;
+
+  C % row_ptr(6) = 17;
+  C % columns(17) = 6; C % columns(18) = 7; C % columns(19) = 8
+
+  C % row_ptr(7) = 20;
+  C % columns(20) = 3; C % columns(21) = 6; C % columns(22) = 7
+  
+  C % row_ptr(8) = 23;
+  C % columns(23) = 1; C % columns(24) = 6; C % columns(25) = 8
+  C % columns(26) = 9; C % columns(27) = 10; 
+
+  C % row_ptr(9) = 28;
+  C % columns(28) = 4; C % columns(29) = 5; 
+  C % columns(30) = 8; C % columns(31) = 9;
+
+  C % row_ptr(10) = 32;
+  C % columns(32) = 2; C % columns(33) = 4; C % columns(34) = 5
+  C % columns(35) = 8; C % columns(36) = 10; 
+
+  C % row_ptr(11) = 37;
+
+  C % values(1:36) = 1.00000
+
+  !call C % print_values
+  !call C % fill_in
+  !stop
 
   call A % init ( 7, 7, 19 ) 
 
@@ -25,7 +154,11 @@ print *, 'sparse matrix tester'
   A % columns(16) = 3; A % columns(17) = 4; A % columns(18) = 5
   A % columns(19) = 7
 
-  A % values(1:19) = CMPLX( 1.00000 , 1.00000)
+  !A % values(1:19) = CMPLX( 1.00000 , 1.00000)
+  A % values(1:19) = 1.00000 
+  call A % print_values
+  call A % fill_in
+  stop
   
   !print *, 'printing values of case 1 matrix'
   !call A % print_values
@@ -85,8 +218,12 @@ print *, 'sparse matrix tester'
   B % values(16) = 13; B % values(17) = 4; B % values(18) = 2
   B % values(19) = -1
 
-  print *, 'printing values of case 2 matrix'
+
   call B % print_values
+  print *
+  call B % fill_in
+  stop
+  print *, 'printing values of case 2 matrix'
   print *
   print *, 'printing values of case 1 matrix augment 1'
   call B % insert( 1, 2, 100.00000_8 )
--- sparse_header.F90
+++ sparse_header.F90
@@ -10,10 +10,11 @@ module sparse_header
 ! matrix stored in contiguous memory locations. The 'columns' array stores the
 ! column indices of the elements of the non-zero array and the 'row_ptr' array
 ! stores stores the indices in the columns and values arrays corresponding to
-! beginning of each row. The total number of elements required is 2*n_nonzero +
-! n + 1.
+! beginning of each row. The total number of elements required is:
+! 4 + 2*n_maximum + (m + 1).
 !
 ! Types are extended for Real*8 and Complex*8 in the values array
+!
 ! SPARSECSRREAL    defines  real(8)   values(:) array
 ! SPARSECSRCOMPLEX defines complex(8) values(:) array
 !
@@ -44,6 +45,9 @@ module sparse_header
   contains
     procedure :: init => sparse_csr_init_real
     procedure :: insert => sparse_csr_insert_real
+    procedure :: fill_in => compute_fill_in_real
+    procedure :: col_below_diag => sparse_column_below_diagonal_real
+    procedure :: row_rgtof_diag => sparse_row_rgtof_diagonal_real
   end type
 
 contains
@@ -80,6 +84,11 @@ contains
 
   end subroutine sparse_csr_init
 
+!===============================================================================
+! SPARSE_CSR_INIT_COMPLEX allocates space for the values array using complex  
+! values, and extends the allocation performed by SPARSE_CSR_INIT
+!===============================================================================
+
   subroutine sparse_csr_init_complex(A, m, n, n_nonzero)
 
     class(SparseCsrComplex) :: A
@@ -98,6 +107,11 @@ contains
 
   end subroutine sparse_csr_init_complex
 
+!===============================================================================
+! SPARSE_CSR_INIT_REAL allocates space for the values array using real
+! values, and extends the allocation performed by SPARSE_CSR_INIT
+!===============================================================================
+
   subroutine sparse_csr_init_real(A, m, n, n_nonzero)
 
     class(SparseCsrReal) :: A
@@ -111,11 +125,110 @@ contains
     if (allocated(A % values)) deallocate(A % values)
     allocate(A % values(A % n_maximum))
 
-    ! type specific to real(8)
+    ! Type specific to real(8)
     A % values = 0.  
 
   end subroutine sparse_csr_init_real   
 
+!===============================================================================
+! SPARSE_ROW_RGTOF_DIAGONAL_REAL
+!===============================================================================
+
+  subroutine sparse_row_rgtof_diagonal_real(A, k, values, nzcols, nnz)
+    class(SparseCsrReal) :: A
+    integer, intent(in)  :: k ! row/column diagonal in square matrix
+    real(8), intent(out) :: values( A % m )
+    integer, intent(out) :: nzcols( A % m ), nnz
+    integer i, first_col_idx, last_col_idx, row_idx, nnz_count
+
+    values = 0.D0
+    nzcols = 0
+    nnz_count = 0
+
+    first_col_idx = A % row_ptr(k)
+    last_col_idx = A % row_ptr(k + 1) - 1
+
+    do i = first_col_idx, last_col_idx
+      if (A % columns(i) > k) then
+        nnz_count = nnz_count + 1
+        values(nnz_count) = A % values(i)
+        nzcols(nnz_count) = A % columns(i)
+      end if
+    end do
+
+    nnz = nnz_count
+
+  end subroutine sparse_row_rgtof_diagonal_real
+
+!===============================================================================
+! SPARSE_COLUMN_BELOW_DIAGONAL_REAL
+!===============================================================================
+
+  subroutine sparse_column_below_diagonal_real(A, k, values, nzrows, nnz)
+    class(SparseCsrReal) :: A
+    integer, intent(in)  :: k ! row/column diagonal in square matrix
+    real(8), intent(out) :: values( A % m )
+    integer, intent(out) :: nzrows( A % m ), nnz
+    integer i, first_col_idx, last_col_idx, row_idx, nnz_count
+
+    values = 0.D0
+    nzrows = 0
+    nnz_count = 0
+
+    do row_idx = k+1, A % m
+
+      first_col_idx = A % row_ptr(row_idx)
+      last_col_idx = A % row_ptr(row_idx + 1) - 1
+
+      do i = first_col_idx, last_col_idx
+        if (k == A % columns(i)) then
+          nnz_count = nnz_count + 1
+          values(nnz_count) = A % values(i)
+          nzrows(nnz_count) = row_idx
+        end if
+      end do
+
+    end do
+
+    nnz = nnz_count
+
+  end subroutine sparse_column_below_diagonal_real
+
+!===============================================================================
+! SPARSE_MAX_BELOW_DIAGONAL
+!===============================================================================
+
+  subroutine sparse_max_below_diagonal_real(A, k, mmaxval, mmaxloc)
+    class(SparseCsrReal) :: A
+    integer, intent(in)  :: k ! column
+    real(8), intent(out) :: mmaxval
+    integer, intent(out) :: mmaxloc
+
+    integer i, first_col_idx, last_col_idx, row_idx
+
+    mmaxval = 0.D0
+    mmaxloc = 0
+
+    do row_idx = 1, A % m
+
+      ! Evaluate the columns that are zero in the row of interest
+      first_col_idx = A % row_ptr(row_idx)
+      last_col_idx = A % row_ptr(row_idx + 1) - 1
+
+      do i = first_col_idx, last_col_idx
+      
+        if (k == A % columns(i)) then
+          if (abs(A % values(i)) > mmaxval) then
+            mmaxval = abs(A % values(i))
+            mmaxloc = row_idx
+          end if
+        end if
+
+      end do
+
+    end do
+
+  end subroutine
 !===============================================================================
 ! SPARSE_CSR_INSERT inserts a new value to csr sparse matrix. This is used when
 ! computing fill-in elements. A new value requires inserting to columns(:),
@@ -128,7 +241,6 @@ contains
     class(SparseCsr) :: A     ! sparse matrix
     integer, intent(in)     :: row   ! row to insert using reg. matrix notation
     integer, intent(in)     :: col   ! col to insert using reg. matrix notation
-    !complex, intent(in)     :: val   ! value of item         
     integer, intent(out)    :: current_col_idx
     integer i, first_col_idx, last_col_idx
     logical at_zero_position
@@ -146,20 +258,20 @@ contains
         print *, 'invalid insert at non-zero location'
         stop
 
-      ! if the col pos is <  'least' column
+      ! If the col pos is <  'least' column
       else if (current_col_idx == first_col_idx .and. &
                col < A % columns(current_col_idx)) then
 
         at_zero_position = .true.; exit
 
-      ! if the col pos is somewhere in between
+      ! If the col pos is somewhere in between
       else if (current_col_idx > 1 .and. &
                col < A % columns(current_col_idx) .and. &
                col > A % columns(current_col_idx-1)) then
 
         at_zero_position = .true.; exit
 
-      ! if the col pos is >  'most' column
+      ! If the col pos is >  'most' column
       else if (current_col_idx == last_col_idx .and. &
                col > A % columns(current_col_idx)) then
         current_col_idx = current_col_idx + 1
@@ -173,20 +285,20 @@ contains
     end do
 
     if ( at_zero_position ) then
-      ! increase nonzero size
+      ! Increase nonzero size
       A % n_nonzero = A % n_nonzero + 1 
 
-      ! shift all row pointers that are +1 from the row insertion
+      ! Shift all row pointers that are +1 from the row insertion
       shift_row_ptr : do i = row + 1, A % m + 1
         A % row_ptr(i) = A % row_ptr(i) + 1    
       end do shift_row_ptr
 
-      ! shift all columns from the end to the current col idx pos + 1
+      ! Shift all columns from the end to the current col idx pos + 1
       shift_col_pos: do i = A % n_nonzero, current_col_idx + 1, -1
         A % columns(i) = A % columns(i-1)
       end do shift_col_pos
       
-      ! the insert goes here
+      ! The insert goes here
       A % columns(current_col_idx) = col           
 
     else
@@ -195,6 +307,11 @@ contains
     end if
 
   end subroutine sparse_csr_insert
+
+!===============================================================================
+! SPARSE_CSR_INSERT_COMPLEX extends on SPARSE_CSR_INSERT
+! The inputs are traditional row,col,val (regular matrix triplet)
+!===============================================================================
 
   subroutine sparse_csr_insert_complex(A, row, col, val)
 
@@ -213,6 +330,11 @@ contains
 
   end subroutine sparse_csr_insert_complex
 
+!===============================================================================
+! SPARSE_CSR_INSERT_REAL extends on SPARSE_CSR_INSERT
+! The inputs are traditional row,col,val (regular matrix triplet)
+!===============================================================================
+
   subroutine sparse_csr_insert_real(A, row, col, val)
 
     class(SparseCsrReal)    :: A     ! sparse matrix
@@ -230,5 +352,153 @@ contains
 
   end subroutine sparse_csr_insert_real   
 
+!===============================================================================
+! COMPUTE_FILL_IN analyzes a csr sparse matrix (real)
+! Needs VALUE_IN_ARRAY function
+!===============================================================================
 
+  function value_in_array(val, array, array_size) result(bool)
+    integer, intent(in) :: val, array_size
+    integer, intent(in) :: array(array_size)
+    logical :: bool
+    integer :: i
+    
+    bool = .FALSE.
+    do i = 1, array_size
+      if (val == array(i)) bool = .TRUE.
+    end do
+
+  end function
+
+  subroutine compute_fill_in_real(A)
+
+    class(SparseCsrReal) :: A ! sparse matrix 
+    integer :: i,j,k, threshold, first_col_idx, last_col_idx
+    integer :: set_size, set(A % n) ! columns in A
+    integer :: front_size, front(A % n) ! columns in A
+    integer :: fill_size, fill(A % n)
+    logical :: already_added, already_in_set
+
+    ! Pre-initialization
+    set = 0
+
+    ! v -------> w is the directed graph
+    ! Wish to find some intermediate u, 
+    ! Such that  v -----> u -----> w
+    ! And that the vertex value of v < both u and w
+
+    ! Initialization let set = A(v), threshold = 1 and F(v) = null
+    ! p. 182 Tarjan & Rose
+
+    ! Define 'set' as the vertices that are in current row
+
+    ! For threshold := 1 until n
+    do threshold = 1, A % n  
+
+      ! Clear set and fill
+      set_size = 0; fill_size = 0
+      set = 0     ; fill = 0
+
+      ! Record all vertices w reachable from v
+
+      ! Evaluate the columns that are zero in the row of interest
+      first_col_idx = A % row_ptr(threshold)
+      last_col_idx = A % row_ptr(threshold + 1) - 1
+
+      ! print *, first_col_idx, last_col_idx
+      do i = first_col_idx, last_col_idx
+        set_size = set_size + 1
+        set(set_size) = A % columns(i)
+      end do
+
+      ! j is the working threshold value, up to threshold
+      ! i is the working element in set or fill arrays
+      ! k is the evaluation of the front array
+
+      ! The work loops are nearly identical, but the 2nd work loop moves through
+      ! the fill array determined in the 1st work loop
+
+      do j = 1, threshold 
+
+        ! work through the set array
+        WORK_LOOP_1: do i = 1, set_size
+          if (j /= threshold) then
+            if (j == set(i)) then
+
+              ! Evaluate the columns that are zero in the row of interest
+              first_col_idx = A % row_ptr(j)
+              last_col_idx = A % row_ptr(j + 1) - 1
+
+              front_size = 0
+              front = 0
+              do k = first_col_idx, last_col_idx
+                front_size = front_size + 1
+                front(front_size) = A % columns(k)
+                if (front(front_size) > j .and. &
+                    threshold /= front(front_size)) then
+
+                  ! Add front value if not already added
+                  already_added = &
+                    value_in_array(front(front_size),fill,fill_size)
+
+                  ! Ensure front value is not in set
+                  already_in_set = &
+                    value_in_array(front(front_size),set,set_size)
+
+                  if (.not. already_added .and. .not. already_in_set) then
+                    fill_size = fill_size + 1
+                    fill(fill_size) = front(front_size) 
+                  end if
+                end if
+              end do
+            end if
+          end if
+        end do WORK_LOOP_1
+
+        ! work through the fill array
+        WORK_LOOP_2: do i = 1, fill_size
+          if (j /= threshold) then
+            if (j == fill(i)) then
+
+              ! Evaluate the columns that are zero in the row of interest
+              first_col_idx = A % row_ptr(j)
+              last_col_idx = A % row_ptr(j + 1) - 1
+
+              front_size = 0
+              front = 0
+              do k = first_col_idx, last_col_idx
+                front_size = front_size + 1
+                front(front_size) = A % columns(k)
+
+                if (front(front_size) > j .and. &
+                    threshold /= front(front_size)) then
+
+                  ! Add front value if not already added
+                  already_added = &
+                    value_in_array(front(front_size),fill,fill_size)
+
+                  ! Ensure front value is not in set
+                  already_in_set = &
+                    value_in_array(front(front_size),set,set_size)
+
+                  if (.not. already_added .and. .not. already_in_set) then
+                    fill_size = fill_size + 1
+                    fill(fill_size) = front(front_size) 
+                  end if
+
+                end if
+              end do
+
+            end if
+          end if
+        end do WORK_LOOP_2
+
+      end do
+
+      if (fill_size > 0) print *, 'fill array:', fill, ' row:', threshold
+
+    end do
+
+  end subroutine compute_fill_in_real
+ 
 end module sparse_header
